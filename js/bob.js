c8d55ba438bdb7e000e428db00973c07d7bac5e6
luckymurphydevelopment <luckymurphydevelopment@gmail.com>
2020-11-11 20:43:55 -0700

    stuff

--- js/bob.js
+++ js/bob.js
@@ -7,47 +7,59 @@ class Bob {
         this.radius = radius;
         this.velocity_x = 0;
         this.velocity_y = 0;
-        this.x = this.pivot.x + (radius * Math.cos(theta));
-        this.y = this.pivot.y + (radius * Math.sin(theta));
+        this.cw = false;
+
+        let coords = calculate_xy(theta, radius);
+        this.x = this.pivot.x + coords[0];
+        this.y = this.pivot.y + coords[1];
         let self = this;
-        
 
         this.clock = setInterval(function () {
-            const G = 9.8;
-            self.theta = calculate_theta(self.x, self.y, 360, 260);
-            let acceleration_vector = G * self.mass * Math.sin(self.theta);
-            let acceleration_x = -acceleration_vector * Math.cos(self.theta);
-            let acceleration_y = acceleration_vector * Math.sin(self.theta);
-            self.velocity_x = acceleration_x;
-            self.velocity_y = acceleration_y;
-            self.x += self.calculate_velocity_x(self, self.x, self.y, self.pivot.x, self.pivot.y) / 10;
-            self.y += self.calculate_velocity_y(self, self.x, self.y, self.pivot.x, self.pivot.y) / 10;
+            self.theta = calculate_theta(self.x, self.y, self.pivot.x, self.pivot.y);
+            let coords = calculate_xy(self.theta, self.radius);
+            self.x = self.pivot.x + coords[0] - self.calculate_velocity_x() / 10;
+            if (Math.abs(self.calculate_velocity_x()) < 0.01) {
+                if (self.x < self.pivot.x) {
+                    self.cw = true;
+                } else {
+                    self.cw = false;
+                }
+            }
+            if (self.cw) {
+                self.y = self.pivot.y + coords[1] + self.calculate_velocity_y() / 10;
+            } else {
+                self.y = self.pivot.y + coords[1] - self.calculate_velocity_y() / 10;
+            }
             self.painter.paint();
         }, 10);
     }
 
     draw(ctx) {
-        ctx.fillText(this.theta, 100, 100);
+        ctx.strokeStyle = "black";
+        // ctx.moveTo(this.x, (this.y > this.pivot.y) ? this.y : this.pivot.y * 2 - this.y);
+        // ctx.lineTo(this.pivot.x, this.pivot.y);
+        // ctx.stroke();
         ctx.fillStyle = "red";
         ctx.beginPath();
+        // ctx.arc(this.x, (this.y > this.pivot.y) ? this.y : this.pivot.y * 2 - this.y, 15, 0, Math.PI * 2);
         ctx.arc(this.x, this.y, 15, 0, Math.PI * 2);
         ctx.fill();
     }
 
     calculate_tension() {
-        return 9.8 * this.mass * Math.cos(this.theta);
+        return 9.8 * this.mass * Math.abs(Math.cos(this.theta));
     }
 
-    calculate_velocity_x() { 
-        return this.calculate_velocity() * Math.cos(calculate_theta(this.x, this.y, this.pivot.x, this.pivot.y));
-    }
-    
-    calculate_velocity_y() { 
-        return this.calculate_velocity() * Math.sin(calculate_theta(this.x, this.y, this.pivot.x, this.pivot.y));
+    calculate_velocity_x() {
+        return this.calculate_velocity() * Math.cos(this.theta);
     }
 
-    calculate_velocity() { 
-        return Math.sqrt(this.radius * this.calculate_tension());
+    calculate_velocity_y() {
+        return this.calculate_velocity() * Math.sin(this.theta);
+    }
+
+    calculate_velocity() {
+        return Math.sqrt(this.radius * this.calculate_tension() / this.mass);
     }
 }
 
@@ -69,5 +81,8 @@ function calculate_theta(x, y, pivot_x, pivot_y) {
     return theta;
 }
 
-
-
+function calculate_xy(theta, radius) {
+    let x = Math.sin(theta) * radius;
+    let y = Math.cos(theta) * radius;
+    return [x, y];
+}
--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ window.onload = function() {
     var pivot = new Pivot(360, 260);
     var bob = new Bob(painter, 1, pivot, Math.PI / 3, 100);
 
-    painter.addObject(pivot);
     painter.addObject(bob);
+    painter.addObject(pivot);
 };
